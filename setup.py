59124a9a1474e0b603161acf0880af26114d08b7
Michael Truog <mjtruog@gmail.com>
2014-12-20 20:22:52 -0800

    Version 1.4.0.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Distributed Computing',
     ],
-    version='1.3.3',
+    version='1.4.0',
     description='Erlang Binary Term Format for Python',
     author='Michael Truog',
     author_email='mjtruog@gmail.com',
