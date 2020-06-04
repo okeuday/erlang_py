98a620061394811fee6a0ed28b2f66a9da7c294f
Michael Truog <mjtruog@protonmail.com>
2020-06-04 00:59:46 -0700

    Version 2.0.0.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Distributed Computing',
     ],
-    version='1.8.0',
+    version='2.0.0',
     description='Erlang Binary Term Format for Python',
     long_description=long_description,
     long_description_content_type='text/x-rst',
